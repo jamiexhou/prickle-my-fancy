49a9fb8ddc544b62adcdfd70a7c648d7b06d6ad8
jamiexhou <jamiexhou@gmail.com>
2022-03-14 18:10:25 -0400

    added content to home, about, and 1st blog post

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,18 @@ import Layout from '../components/layout'
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+        <p>My name is Jamie and I am a 22 year old torontonian. As someone who spends a layout
+          of time outside, I was really slowly being driven insane thanks to the quarantines. So 
+          I dedicated my frenzied energy into figuring out how to bring the outside, well, inside!
+          I'm going to attempt to write weekly ish blog posts about the state of my plants, new plant 
+          projects, updates on projects, things I'm trying out, and just whatever strikes my fancy. 
+        </p>
+        <p>I am excited to share all of my musing with everyone else in the plant community and 
+          hopefully together we can grow bigger and happier plants!
+        </p>
+        <p>This project was built using Gatsby and Github Pages. I would like to thank everyone
+          who helped me with this project and put up with me as I struggled.
+        </p>
     </Layout>
   )
 }
