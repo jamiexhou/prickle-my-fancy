2c2ea6ae55fc98f6f31e3690a807e59670e99b2f
jamiexhou <40585387+jamiexhou@users.noreply.github.com>
2022-03-24 00:20:22 -0400

    changed formatting. MDX embeded is also not working, replaced with an alternative (#4)
    resolving git conflicts

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-mdx-embed",
+    "gatsby-plugin-mdx-embed"
   ],
-  pathPrefix: "/prickle-my-fancy",
+  pathPrefix: "/prickle-my-fancy"
 }
